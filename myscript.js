8b85d345243d2d8149198eaba80420c27365657b
mattesalva93 <salvalaggiomatteo93@gmail.com>
2022-01-14 14:10:05 +0100

    v-for not working

---
+++ myscript.js
@@ -0,0 +1,34 @@
+let app = new Vue({
+    el: "#container",
+    data: {
+        elenco : [
+            {
+                pic: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                pic: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum',
+            },	
+            {
+                pic: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                pic: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+            {
+                pic: 'img/05.jpg',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+        ],
+
+
+    }
+});
